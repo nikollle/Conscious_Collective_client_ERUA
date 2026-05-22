a75e79c628d45191149e668dcc6710539577a24d
Rea Tsenkova <rea.tsenkova@email.com>
2026-05-22 12:12:53 +0300

    added template content inside empty folders

---
+++ src/app/sign-up.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SignUp() {
+  return (
+    <View className="flex-1 items-center justify-center bg-greige">
+      <Text className="text-oxblood text-xl">Sign Up</Text>
+    </View>
+  );
+}
